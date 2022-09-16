f40871cd3246249133b6c167c6164deb05e77f37
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2022-09-15 22:06:19 -0300

    Aula5-front finalizado

--- web/src/components/Form/Input.tsx
+++ web/src/components/Form/Input.tsx
@@ -5,7 +5,9 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 export function Input(props: InputProps) {
   return (
     <input
-      className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
+      className={`bg-zinc-900 py-3 ${
+        props.type === "time" ? "px-1 pl-4" : "px-4"
+      } rounded text-sm placeholder:text-zinc-500`}
       {...props}
     />
   );
